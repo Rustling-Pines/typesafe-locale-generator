e685806ffa58866bdd7b5645d96023b67f0acacd
Pradeep Rajendran <rajendran@porziols.com>
2024-12-08 12:37:53 -0500

    added place holder for examples

--- src/translations/messages/welcome.ts
+++ src/translations/messages/welcome.ts
@@ -2,10 +2,10 @@ import { Locales } from ".."
 import { ITranslations } from "../../types/ITranslations"
 
 export const WelcomeMessage: ITranslations<Locales> = {
-    key: 'WELCOME',
-    'en-us': 'Welcome',
-    fr: 'Bienvenue',
-    de: 'Willkommen',
-    in: 'स्वागत हे',
-    jp: 'ようこそ',
+    key: 'WELCOME_MESSAGE',
+    'en-us': 'Welcome, {name}!',
+    fr: 'Bienvenue, {name}!',
+    de: 'Willkommen, {name}!',
+    in: 'स्वागत है, {name}!',
+    jp: '{name} さん、ようこそ！',
 }
